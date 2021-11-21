ffcf0e84a2f40091ccc8e198664ca3d70a47df49
bleeq <bleeqtheproducer@gmail.com>
2021-11-21 18:02:47 +0900

    조건 검색 기능 구현

--- src/test/java/com/bleeqer/mapper/BoardmapperTests.java
+++ src/test/java/com/bleeqer/mapper/BoardmapperTests.java
@@ -84,18 +84,30 @@ public class BoardmapperTests {
 //		log.info("UPDATE COUNT: " + count);
 //	}
 	
+//	@Test
+//	public void testPaging() {
+//		
+//		Criteria cri = new Criteria();
+//		
+//		cri.setPageNum(3);
+//		cri.setAmount(10);
+//		
+//		List<BoardVO> list = mapper.getListWithPaging(cri);
+//		
+//		list.forEach(board -> log.info(board));
+//		
+//	}
+	
 	@Test
-	public void testPaging() {
+	public void testSearch() {
 		
 		Criteria cri = new Criteria();
-		
-		cri.setPageNum(3);
-		cri.setAmount(10);
+//		cri.setKeyword("Å×½ºÆ®");
+//		cri.setType("TC");
 		
 		List<BoardVO> list = mapper.getListWithPaging(cri);
 		
-		list.forEach(board -> log.info(board));
-		
+		list.forEach(board-> log.info(board));
 	}
 }
 	
